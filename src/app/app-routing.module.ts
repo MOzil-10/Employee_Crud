7c33f09481a68ea896fb4fb7e7c5bf682a244071
MOzil-10 <mukosibudeli@gmail.comm>
2024-03-05 12:40:56 +0200

    Added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home/home.component';
+import { RegisterComponent } from './register/register/register.component';
+import { LoginComponent } from './login/login/login.component';
+import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:HomeComponent},
+  {path: 'register', component:RegisterComponent},
+  {path: 'login', component:LoginComponent},
+  {path: 'dashboard', component:DashboardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
