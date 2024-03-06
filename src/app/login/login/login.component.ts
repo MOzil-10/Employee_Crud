856b3ded07de709eba5729db3674911d3f1a5a53
MOzil-10 <mukosibudeli@gmail.comm>
2024-03-06 11:30:13 +0200

    Register and login forms

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,58 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+  loginForm!: FormGroup;
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.loginForm.valid) {
+      // Here you can implement your login logic
+      console.log("Login successful");
+    }
+  }
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+  getErrors(controlName: string): string[] {
+    const control = this.loginForm.get(controlName);
+    const errors: string[] = [];
+    if (control && control.errors) {
+      for (const errorName in control.errors) {
+        if (control.errors.hasOwnProperty(errorName)) {
+          switch (errorName) {
+            case 'required':
+              errors.push('Field is required');
+              break;
+            case 'email':
+              errors.push('Invalid email');
+              break;
+            case 'minlength':
+              errors.push('Password must be at least 6 characters long');
+              break;
+            default:
+              break;
+          }
+        }
+      }
+    }
+    return errors;
+  }
 
 }
--- src/app/register/register/register.component.ts
+++ src/app/register/register/register.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
 
+  registerForm!: FormGroup;
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder){}
+
+  ngOnInit(): void {
+    //Validators
+    this.registerForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      department: ['', Validators.required],
+      password: ['', [Validators.required, this.validatePassword]],
+      confirmPassword: ['', Validators.required],
+    }, {validator: this.passwordMatchValidator});
+  }
+
+  onSubmit(){
+    this.submitted = true;
+    if(this.registerForm.valid){
+      alert("Successfully registered");
+    }
+  }
+
+  validatePassword(control: AbstractControl): {[key: string]: any} | null {
+    const password = control.value;
+    const hasNumber = /\d/.test(password);
+    const hasUpper = /[A-Z]/.test(password);
+    const hasLower = /[a-z]/.test(password);
+    const hasSpecial = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password);
+    const isValid = hasNumber && hasUpper && hasLower && hasSpecial && password.length >= 8;
+    return isValid ? null : {invalidPassword: true};
+  }
+
+  passwordMatchValidator(control: AbstractControl): {[key: string]: any} | null {
+    const password = control.get('password')?.value;
+    const confirmPassword = control.get('confirmPassword')?.value;
+    return password === confirmPassword ? null : {passwordMismatch: true};
+  }
 }
